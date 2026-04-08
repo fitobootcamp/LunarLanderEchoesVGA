6ceae47fefc3ec51a82e7b2b3a79ba71b2a1f64a
fitobootcamp <fitojuegos54@gmail.com>
2026-04-08 04:10:09 -0600

    less gooo

--- lunar-lander-echoes-vga/combustible.gd
+++ lunar-lander-echoes-vga/combustible.gd
@@ -4,11 +4,11 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
-	$ProgressBar.value = navebody.combustible
+	$TextureProgressBar.value = navebody.combustible
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$ProgressBar.value = navebody.combustible
-	pass
+	$TextureProgressBar.value = navebody.combustible
+	
